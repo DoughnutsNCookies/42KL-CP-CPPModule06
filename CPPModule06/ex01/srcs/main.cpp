818212ae4cdd95d898646dc9088791ac3168d11b
Sean Chuah <chuahtseyung2002@gmail.com>
2023-07-17 19:50:43 +0800

    bug fix

--- CPPModule06/ex01/srcs/main.cpp
+++ CPPModule06/ex01/srcs/main.cpp
@@ -3,31 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: schuah <schuah@student.42kl.edu.my>        +#+  +:+       +#+        */
+/*   By: schuah <schuah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 17:37:45 by schuah            #+#    #+#             */
-/*   Updated: 2022/09/01 18:18:59 by schuah           ###   ########.fr       */
+/*   Updated: 2023/07/17 19:49:58 by schuah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.hpp"
 
-uintptr_t	serialize(Data *ptr)
+uintptr_t serialize(Data *ptr)
 {
 	return (reinterpret_cast<uintptr_t>(ptr));
 }
 
-Data	*deserialize(uintptr_t raw)
+Data *deserialize(uintptr_t raw)
 {
 	return (reinterpret_cast<Data *>(raw));
 }
 
 /* Reinterpret_cast typecasts any pointer to any other pointer */
-int	main(void)
+int main(void)
 {
-	Data		data;
-	Data		*dataPointer;
-	uintptr_t	unsignedIntPointer;
+	Data data;
+	Data *dataPointer;
+	uintptr_t unsignedIntPointer;
 
 	data.name = "Sean";
 	data.age = 20;
@@ -40,10 +40,11 @@ int	main(void)
 	std::cout << "Name: " << data.name << std::endl;
 	std::cout << "Age: " << data.age << std::endl;
 	std::cout << "Pointer: " << &data << std::endl;
+	std::cout << "Unsigned int pointer: " << &unsignedIntPointer << std::endl;
 	dataPointer = deserialize(unsignedIntPointer);
 	std::cout << "\n---------- EX01 Deserialized ----------\n" << std::endl;
 	std::cout << "Name: " << dataPointer->name << std::endl;
 	std::cout << "Age: " << dataPointer->age << std::endl;
-	std::cout << "Pointer: " << &dataPointer << std::endl;
+	std::cout << "Pointer: " << dataPointer << std::endl;
 	return (0);
 }
